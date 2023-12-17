277c0e6ad9917649d5359f328ec0baa35b30cc8b
Chetan Yadav <ydvchetan01@gmail.com>
2023-12-17 12:30:18 +0530

    Create program02.py

---
+++ program02.py
@@ -0,0 +1,34 @@
+# Single-Line Comment
+# print("Hello World")
+# Hey Chetan, Please don't remove this line
+
+print("Hello World") # Printing Hello World
+
+# Multi-Line Comment
+# Python is commonly used for developing websites and software,
+# task automation, data analysis, and data visualization.
+# Ctrl + /
+
+
+'''
+Python is a multi-paradigm programming language. Object-oriented 
+programming and structured programming are fully supported, and
+'''
+
+"""
+many of their features support functional programming and 
+aspect-oriented programming (including metaprogramming and metaobjects).
+"""
+
+print("Hey I am a good boy\nand this viewer is also a good boy/girl")
+
+# print("Hey I am a "good boy"\nand this viewer is also a good boy/girl")
+
+print("Hey I am a \"good boy\"\nand this viewer is also a good boy/girl")
+
+print('Hey I am\' a \"good boy\"\nand this viewer is also a good boy/girl')
+
+print('Hey I am\' a "good boy"\nand this viewer is also a good boy/girl')
+
+print("Hey", 6, 7, sep="~", end="009\n")
+print("Chetan")
