7855436ebc872740b5c1999e2b8700b94169f251
Chetan Yadav <ydvchetan01@gmail.com>
2023-12-18 22:59:07 +0530

    Create program03.py and program04.py

---
+++ program03.py
@@ -0,0 +1,18 @@
+a = 1
+print(a)
+b = "Chetan"
+print(b)
+c = None
+print(c)
+d = True
+print(d)
+e = 1.1
+print(e)
+f = complex(8, 2)
+print(f)
+print("The type of a is", type(a))
+print("The type of b is", type(b))
+print("The type of c is", type(c))
+print("The type of d is", type(d))
+print("The type of e is", type(e))
+print("The type of f is", type(f))
