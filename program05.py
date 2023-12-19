2369d28cb49179f7878dda982c4a5ba1178146f6
Chetan Yadav <ydvchetan01@gmail.com>
2023-12-19 19:34:03 +0530

    Create program05.py and program06.py

---
+++ program05.py
@@ -0,0 +1,17 @@
+c = "Chetan"
+d = "Yadav"
+print(c + d)
+
+a = "1"
+b = "2"
+print(a + b)
+print(int(a) + int(b))
+
+e = 1
+f = 2
+print(e + f)
+
+# Implicit Typecasting
+x = 1.9
+y = 8
+print(x + y)
---
+++ program06.py
@@ -0,0 +1,13 @@
+a = input()
+print("My name is", a)
+
+b = input("Enter you name: ")
+print("My name is", b)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+print(x + y)
+print(int (x) + int (y))
+
+variable=int(input())
+variable=float(input())
