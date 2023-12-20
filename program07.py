4157da28e4ff0a0b8cea36c4837427a1accd2111
Chetan Yadav <ydvchetan01@gmail.com>
2023-12-20 08:26:18 +0530

    Create program07.py

---
+++ program07.py
@@ -0,0 +1,32 @@
+name = "Chetan"
+friend = "Virat"
+anotherfriend = 'Rahul'
+print("Hello, " + name)
+print("Hello,", name)
+apple = "He said, \"I want to eat an apple\"."
+
+apple = "He said, 'I want to eat an apple'."
+
+apple = 'He said, "I want to eat an apple".'
+
+car = """I am,
+we are,
+you are"""
+print(car)
+
+ball = '''He said,
+hi Chetan
+hey I am good
+"I want to eat an apple"'''
+# print(ball, "\n")
+print(ball)
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+
+for character in ball:
+    print(character)
